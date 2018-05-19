84fad7738988e7f6d7cb88a566140f1f97787b02
peng <244757702@qq.com>
2018-05-19 04:02:10 -0700

    hello

---
+++ hello.java
@@ -0,0 +1,10 @@
+public class hello{
+	public static void main(String[] args){
+	for(int i=0;i<=9;i++){
+		for(int j=1;j<=i;j++){
+		System.out.println(j+"x"+i+"="+i*j);
+		}
+	}
+	System.out.println("hello");
+	}
+}
